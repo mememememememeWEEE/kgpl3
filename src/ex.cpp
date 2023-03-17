7e9f90f8d52ece57c5b948bcf4f2c0877a55eb36
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-03-17 16:26:40 -0700

    .

--- src/ex.cpp
+++ src/ex.cpp
@@ -6,7 +6,7 @@ int main(void)
         1200, 800, "Example Game"
     });
 
-    auto h = KGPL::CompileVertexShader("h", true);
+    auto h = KGPL::ShaderUtil::CompileVertexShader("h", true);
 
     /* Loop until the user closes the window */
     for (int i = 0; i < 100000; i++) {
