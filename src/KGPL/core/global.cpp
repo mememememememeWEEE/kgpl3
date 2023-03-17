d775b044426b9311ef3c97cd9a83bbd3649e7cf4
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-03-17 15:23:01 -0700

    .

--- src/KGPL/core/global.cpp
+++ src/KGPL/core/global.cpp
@@ -8,12 +8,11 @@ namespace KGPL {
 		uint32_t WindowHeight;
 
 		GLFWwindow* Window;
+
+		GLuint ShaderProgram2D;
+		GLuint ShaderProgram3D;
+
+		GLuint CurVertexShader;
+		GLuint CurFragmentShader;
 	}
 }
-
-bool Initialized = false;
-
-uint32_t WindowWidth;
-uint32_t WindowHeight;
-
-GLFWwindow* Window;
--- src/KGPL/core/global.hpp
+++ src/KGPL/core/global.hpp
@@ -2,13 +2,25 @@
 #define KGPL_GLOBAL_H
 #include "../kgpl.h"
 
-namespace KGPL { namespace PRIVATE {
-	extern bool Initialized;
+namespace KGPL {
+	namespace PRIVATE {
+		extern bool Initialized;
 
-	extern uint32_t WindowWidth;
-	extern uint32_t WindowHeight;
+		extern uint32_t WindowWidth;
+		extern uint32_t WindowHeight;
 
-	extern GLFWwindow* Window;
-} };
+		extern GLFWwindow* Window;
+
+		extern GLuint ShaderProgram2D;
+		extern GLuint ShaderProgram3D;
+
+		extern GLuint CurVertexShader;
+		extern GLuint CurFragmentShader;
+	}
+
+	[[nodiscard]] inline auto getWindow() noexcept {
+		return KGPL::PRIVATE::Window;
+	}
+};
 
 #endif
