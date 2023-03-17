7709f286591aa23aa55b84b00e8c931f969d3ca6
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-03-17 16:25:32 -0700

    .

--- src/KGPL/core/global.cpp
+++ src/KGPL/core/global.cpp
@@ -9,10 +9,15 @@ namespace KGPL {
 
 		GLFWwindow* Window;
 
-		GLuint ShaderProgram2D;
-		GLuint ShaderProgram3D;
+		GLuint ShaderProgram2D = NULL;
+		GLuint ShaderProgram3D = NULL;
 
-		GLuint CurVertexShader;
-		GLuint CurFragmentShader;
+		GLuint CurVertexShader2D = NULL;
+		GLuint CurFragmentShader2D = NULL;
+		GLuint CurGeometryShader2D = NULL;
+
+		GLuint CurVertexShader3D = NULL;
+		GLuint CurFragmentShader3D = NULL;
+		GLuint CurGeometryShader3D = NULL;
 	}
 }
--- src/KGPL/core/global.hpp
+++ src/KGPL/core/global.hpp
@@ -13,14 +13,58 @@ namespace KGPL {
 
 		extern GLuint ShaderProgram2D;
 		extern GLuint ShaderProgram3D;
-
-		extern GLuint CurVertexShader;
-		extern GLuint CurFragmentShader;
+		
+		extern GLuint CurVertexShader2D;
+		extern GLuint CurFragmentShader2D;
+		extern GLuint CurGeometryShader2D;
+		
+		extern GLuint CurVertexShader3D;
+		extern GLuint CurFragmentShader3D;
+		extern GLuint CurGeometryShader3D;
 	}
 
+#pragma region getters
 	[[nodiscard]] inline auto getWindow() noexcept {
 		return KGPL::PRIVATE::Window;
 	}
+#pragma endregion
+
+#pragma region setters
+
+	inline void setShaderProgram2D(GLuint program) {
+		KGPL::PRIVATE::ShaderProgram2D = program;
+	}
+
+	inline void setShaderProgram3D(GLuint program) {
+		KGPL::PRIVATE::ShaderProgram3D = program;
+	}
+
+	inline void setVertexShader2D(GLuint shader) {
+		KGPL::PRIVATE::CurVertexShader2D = shader;
+	}
+
+	inline void setFragmentShader2D(GLuint shader) {
+		KGPL::PRIVATE::CurFragmentShader2D = shader;
+	}
+
+	inline void setGeometryShader2D(GLuint shader) {
+		KGPL::PRIVATE::CurGeometryShader2D = shader;
+	}
+
+	inline void setVertexShader3D(GLuint shader) {
+		KGPL::PRIVATE::CurVertexShader3D = shader;
+	}
+
+	inline void setFragmentShader3D(GLuint shader) {
+		KGPL::PRIVATE::CurFragmentShader3D = shader;
+	}
+
+	inline void setGeometryShader3D(GLuint shader) {
+		KGPL::PRIVATE::CurGeometryShader3D = shader;
+	}
+
+#pragma endregion
+
 };
 
 #endif
