d49c181e5b903c365f1b2a38207562196a66c209
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-03-18 11:39:19 -0700

    .

--- src/KGPL/render/renderer_init.cpp
+++ src/KGPL/render/renderer_init.cpp
@@ -1,5 +1,8 @@
 #include "renderer_init.hpp"
 
 bool KGPL::Render::RendererInit() {
+	system("echo %cd%");
+	KGPL::setVertexShader2D(KGPL::Render::ShaderUtil::CompileVertexShaderFromFile(R"(../shader/Vert2D.glsl)", true));
+	KGPL::setFragmentShader2D(KGPL::Render::ShaderUtil::CompileFragmentShaderFromFile(R"(C:\Users\rando\OneDrive\Documents\GitHub\kgpl3\shader\Frag2D.glsl)", true));
 	return true;
 }
